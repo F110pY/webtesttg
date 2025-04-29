1ba016917f27f3942e79ce37ae044bf23cd77d80
FllopY <proofdeser2748@gmail.com>
2025-04-29 18:29:23 +0300

    wqwe

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot('7698724074:AAFsRqm-FP5B0vul9xYsmBzJ2Ap7wae7HsE')
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     markup = types.InlineKeyboardMarkup()
-    button = types.InlineKeyboardButton('Hello', web_app=WebAppInfo(url='https://f110py.github.io/webtesttg/'))
+    button = types.InlineKeyboardButton('Hello11git1', web_app=WebAppInfo(url='https://f110py.github.io/webtesttg/'))
     markup.add(button)
     bot.send_message(message.chat.id, 'Hello', reply_markup=markup)
 
