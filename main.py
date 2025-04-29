ff6c92d0fa8bd0c812d519a59698d081e42017e1
FllopY <proofdeser2748@gmail.com>
2025-04-29 18:45:24 +0300

    wqwe

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ bot = telebot.TeleBot('7698724074:AAFsRqm-FP5B0vul9xYsmBzJ2Ap7wae7HsE')
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    markup = types.InlineKeyboardMarkup()
-    button = types.InlineKeyboardButton('Hello', web_app=WebAppInfo(url='https://f110py.github.io/webtesttg/'))
+    markup = types.ReplyKeyboardMarkup()
+    button = types.KeyboardButton('Hello', web_app=WebAppInfo(url='https://f110py.github.io/webtesttg/'))
     markup.add(button)
     bot.send_message(message.chat.id, 'Hello', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['web_app_data'])
+def web_app(message):
+    bot.send_message(message.chat.id, message.web_app_data.data)
+
 
 bot.polling(none_stop=True)
